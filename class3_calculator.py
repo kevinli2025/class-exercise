851dc5a50e38a73b829219a75033a7a6f0426e3d
kevinli2025 <kevli2006@gmail.com>
2026-09-18 14:51:22 -0400

    implement subtract

--- class3_calculator.py
+++ class3_calculator.py
@@ -14,13 +14,23 @@ def main():
         default="add",
         help="Operation to perform"
     )
+    parser.add_argument(
+        "--operation", "-op",
+        choices=["add", "subtract"],
+        default="add",
+        help="Operation to perform"
+    )
     args = parser.parse_args()
 
     if args.operation == "add":
         result = add(args.a, args.b)
+    elif args.operation == "subtract":
+        result = subtract(args.a, args.b)
 
     print(f"Result: {result}")
 
+def subtract(a,b):
+    return a - b
 
 if __name__ == "__main__":
     main()
